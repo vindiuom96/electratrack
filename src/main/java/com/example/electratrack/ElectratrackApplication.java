97c341b93853b757004de0dcd13df04a05f7c792
Vindya Perera <vindipererauom@gmail.com>
2025-11-27 17:53:38 +1100

    "Initial commit"

---
+++ src/main/java/com/example/electratrack/ElectratrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.electratrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectratrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectratrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/electratrack/ElectratrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electratrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectratrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
